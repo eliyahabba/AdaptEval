e598537ea1b0d6792e01a8664819cec3ca2a4356
ehabba <ehabba@lightricks.com>
2025-08-27 08:50:34 +0300

    vectorizer matrix build

--- src/llm_eval/training/__init__.py
+++ src/llm_eval/training/__init__.py
@@ -3,9 +3,11 @@ from .irt import (
     train_and_validate_item_parameters,
     select_anchors,
     select_anchors_with_matrix,
+    select_anchors_structured_with_matrix,
     save_item_parameters,
     load_item_parameters,
     save_anchors,
+    save_anchors_structured,
     load_anchors,
 )
 
@@ -14,9 +16,11 @@ __all__ = [
     "train_and_validate_item_parameters",
     "select_anchors",
     "select_anchors_with_matrix",
+    "select_anchors_structured_with_matrix",
     "save_item_parameters",
     "load_item_parameters",
     "save_anchors",
+    "save_anchors_structured",
     "load_anchors",
 ]
 
