8f9ab43302c24c0348ee8b1395e5cc37473a6afd
ehabba <ehabba@lightricks.com>
2025-12-23 15:45:12 +0200

    added anchor-only-fixed

--- sh_run/run_chain_linking_parallel_b.sh
+++ sh_run/run_chain_linking_parallel_b.sh
@@ -5,7 +5,7 @@
 #SBATCH --time=24:0:0
 #SBATCH --mail-user=eliya.habba@mail.huji.ac.il
 #SBATCH --mail-type=END,FAIL,TIME_LIMIT
-#SBATCH --gres=gpu:4
+#SBATCH --gres=gpu:g0:4
 #SBATCH --cpus-per-task=8
 #SBATCH --killable
 #SBATCH --requeue
