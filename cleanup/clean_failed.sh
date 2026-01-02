afc647969372a83299bdbd803bc0fad091fc2f77
ehabba <ehabba@lightricks.com>
2026-01-02 07:55:27 +0200

    removed old data

--- cleanup/clean_failed.sh
+++ cleanup/clean_failed.sh
@@ -1,3 +1,15 @@
 #!/bin/bash
-python clean_failed.py "$1"
 
+#SBATCH --job-name=cleanup
+#SBATCH --mem=2g
+#SBATCH --time=0:30:0
+#SBATCH --mail-user=eliya.habba@mail.huji.ac.il
+#SBATCH --mail-type=END,FAIL
+#SBATCH --cpus-per-task=1
+
+PROJECT_DIR="/cs/labs/gabis/eliyahabba/AdaptEval"
+cd $PROJECT_DIR
+
+source /cs/snapless/gabis/eliyahabba/venvs/AdaptEval/bin/activate
+
+python cleanup/clean_failed.py "$1"
